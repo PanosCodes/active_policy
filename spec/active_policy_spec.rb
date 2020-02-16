b1c4de3bc39148589ddafdd98b73dbf723c21fb1
Panos Dalitsouris <hello@panos.codes>
2020-02-16 13:04:01 +0200

    remove unused spec

--- spec/active_policy_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe ActivePolicy do
-  it "has a version number" do
-    expect(ActivePolicy::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
