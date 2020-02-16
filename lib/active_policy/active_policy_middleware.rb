3b758aa50a602db2e0309306fe188bb0b889110e
Panos Dalitsouris <hello@panos.codes>
2020-02-16 20:25:19 +0200

    update current_user to get the authorized warden user

--- lib/active_policy/active_policy_middleware.rb
+++ lib/active_policy/active_policy_middleware.rb
@@ -1,5 +1,9 @@
+require 'active_policy/utilities/utilities'
 class ActivePolicyMiddleware
-  # @param [Object] app
+  ENV_KEY_REQUEST_METHOD = 'REQUEST_METHOD'
+  ENV_KEY_WARDEN = 'warden'
+
+  # @param [Class] app
   def initialize(app)
     @app = app
   end
@@ -8,14 +12,14 @@ class ActivePolicyMiddleware
   def call(env)
     params = ActivePolicy::Utilities.route_params(
         ActionDispatch::Request.new(env).path,
-        env['REQUEST_METHOD'],
+        env[ENV_KEY_REQUEST_METHOD],
         Rails.application.routes
     )
 
     if params.key?(:policy)
-      policy = params[:policy].new env['Warden']
+      policy = params[:policy].new current_user(env)
       method_name = params[:action] + '?'
-      models = ActivePolicy::Utilities.models_from_route_params(params[:policy_models])
+      models = ActivePolicy::Utilities.models_from_route_params(params)
 
       policy.send(method_name, *models)
     end
@@ -26,10 +30,11 @@ class ActivePolicyMiddleware
   private
 
   # @param [Hash] env
+  #
   # @return [User, nil]
   def current_user(env)
-    if env.key?('Warden')
-      return env['Warden'].user
+    if env.key?(ENV_KEY_WARDEN)
+      return env[ENV_KEY_WARDEN].user
     end
 
     nil
