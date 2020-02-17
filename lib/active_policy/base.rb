4024f619b860f9109ce39836e04f5e82e040aa76
Panos Dalitsouris <hello@panos.codes>
2020-02-17 21:16:11 +0200

    add params and response to base policy

--- lib/active_policy/base.rb
+++ lib/active_policy/base.rb
@@ -2,9 +2,15 @@ module ActivePolicy
   class Base
     # @param [User] current_user
     # @param [ActionDispatch::Request]
-    def initialize(current_user, request)
+    def initialize(current_user, request, params)
       @current_user = current_user
       @request = request
+      @params = params
+    end
+
+    # @param [Rack::Response]
+    def response(status_code, headers = {'content_type': 'application/json'}, body = [])
+      Rack::Response.new(body, status_code, headers)
     end
   end
 end
